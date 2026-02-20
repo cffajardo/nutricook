0d9155bd1f826520dd620d499423b81be1db882c
Cill Fajardo <cillfroilanf@gmail.com>
2026-02-20 22:46:35 +0800

    Removed unneeded imports

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -1,5 +1,4 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import '../features/utils/timestamp_convert.dart';
 import 'nutrition_info.dart';
 import 'recipe_ingredient.dart';
