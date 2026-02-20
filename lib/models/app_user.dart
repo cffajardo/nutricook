972e2cba4f906dfbd6aac9aa58ad4e9fbbe6a768
Cill Fajardo <cillfroilanf@gmail.com>
2026-02-20 22:14:03 +0800

    User Model Created

---
+++ lib/models/app_user.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import '../features/utils/timestamp_convert.dart';
+
+part 'app_user.freezed.dart';
+part 'app_user.g.dart';
+
+@freezed
+abstract class AppUser with _$AppUser {
+  const factory AppUser({
+    required String id,
+    required String username,
+    required String email,
+    @TimestampConverter() required Timestamp createdAt,
+    String? mediaId,
+    @Default(<String>[]) List<String> allergens,
+
+  }) = _AppUser;
+
+  factory AppUser.fromJson(Map<String, dynamic> json) =>
+    _$AppUserFromJson(json);
+}
