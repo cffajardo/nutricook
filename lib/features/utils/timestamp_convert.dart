d3a4382ed458c5a5210617455a6fe89398648f30
Cill Fajardo <cillfroilanf@gmail.com>
2026-02-22 00:50:47 +0800

    Modified Timestamp converter

--- lib/features/utils/timestamp_convert.dart
+++ lib/features/utils/timestamp_convert.dart
@@ -1,12 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-class TimestampConverter implements JsonConverter<Timestamp, Timestamp> {
+class TimestampConverter
+    implements JsonConverter<DateTime, Timestamp> {
   const TimestampConverter();
 
   @override
-  Timestamp fromJson(Timestamp json) => json;
+  DateTime fromJson(Timestamp json) => json.toDate();
 
   @override
-  Timestamp toJson(Timestamp object) => object;
+  Timestamp toJson(DateTime object) =>
+      Timestamp.fromDate(object);
 }
