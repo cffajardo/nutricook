487b260a0bdb86fb9387661fe835e345629b92ea
Cill Fajardo <cillfroilanf@gmail.com>
2026-03-02 17:26:35 +0800

    Add user provider and stream providers for user data and allergens

---
+++ lib/features/profile/provider/user_provider.dart
@@ -0,0 +1,35 @@
+import 'package:nutricook/features/profile/service/user_service.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nutricook/features/auth/providers/auth_provider.dart';
+
+
+final userServiceProvider = Provider<UserService>((ref) {
+  return UserService();
+});
+
+
+
+final userDataProvider = StreamProvider((ref) {
+  final userId = ref.watch(currentUserIdProvider);
+  if (userId == null) return Stream.value(null);
+  
+  final userService = ref.watch(userServiceProvider);
+  return userService.getUserDataStream(userId);
+});
+
+final userAllergenProvider = StreamProvider<List<String>>((ref) {
+  final uid = ref.watch(currentUserIdProvider);
+
+  if (uid == null) {
+    return Stream.value(<String>[]);
+  }
+
+  final userService = ref.read(userServiceProvider);
+  return userService.getUserAllergensStream(uid);
+});
+
+
+
+
+
+
