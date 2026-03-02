ac080826233cd0ff54f18733c6dece8d1927f315
Cill Fajardo <cillfroilanf@gmail.com>
2026-03-02 17:26:26 +0800

    Add calculatePlannerNutrition function to compute total nutrition from planner items

---
+++ lib/features/planner/util/nutrition_info_util.dart
@@ -0,0 +1,32 @@
+import 'package:nutricook/models/nutrition_info/nutrition_info.dart';
+import 'package:nutricook/models/planner_item/planner_item.dart';
+
+NutritionInfo calculatePlannerNutrition({
+  required List<PlannerItem> plannerItems,
+}) {
+  return plannerItems.fold(
+    const NutritionInfo(
+      calories: 0,
+      carbohydrates: 0,
+      protein: 0,
+      fat: 0,
+      fiber: 0,
+      sugar: 0,
+      sodium: 0,
+    ),
+    (total, item) {
+      final n = item.nutritionPerServing;
+      if (n == null) return total;
+      final scale = item.servingMultiplier;
+      return total.copyWith(
+        calories: total.calories + (n.calories * scale).round(),
+        carbohydrates: total.carbohydrates + n.carbohydrates * scale,
+        protein: total.protein + n.protein * scale,
+        fat: total.fat + n.fat * scale,
+        fiber: total.fiber + n.fiber * scale,
+        sugar: total.sugar + n.sugar * scale,
+        sodium: total.sodium + n.sodium * scale,
+      );
+    },
+  );
+}
