cd206fd0df23726d6f5e90614c9edf3f9e6d248c
Cill Fajardo <cillfroilanf@gmail.com>
2026-03-07 14:35:57 +0800

    Refactor main.dart for improved readability and consistency in theme configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nutricook/features/profile/provider/user_preferences_provider.dart';
 import 'firebase_options.dart';
 import 'routing/main_router.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(
-    const ProviderScope(
-      child: NutriCookApp(),
-    ),
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const ProviderScope(child: NutriCookApp()));
 }
 
 class NutriCookApp extends ConsumerWidget {
@@ -21,12 +16,21 @@ class NutriCookApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(appThemeModeProvider);
+
     return MaterialApp.router(
-        title: 'NutriCook',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      title: 'NutriCook',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.orange,
+          brightness: Brightness.dark,
         ),
-        routerConfig: ref.watch(routerProvider),
-      );
+      ),
+      themeMode: themeMode,
+      routerConfig: ref.watch(routerProvider),
+    );
   }
 }
