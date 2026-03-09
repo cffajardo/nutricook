a8e599e7fb14d2c90fc5145c980c2d025c087dea
Cill Fajardo <cillfroilanf@gmail.com>
2026-03-10 00:12:06 +0800

    Add AppRoutes class to define application routing paths and names

---
+++ lib/routing/app_routes.dart
@@ -0,0 +1,22 @@
+class AppRoutes {
+  static const String splashPath = '/splash';
+  static const String homePath = '/';
+  static const String recipesPath = '/recipes';
+  static const String plannerPath = '/planner';
+  static const String loginPath = '/login';
+  static const String registerPath = '/register';
+  static const String verifyEmailPath = '/verify-email';
+  static const String subCategoryPath = '/recipes/:category';
+  static const String recipeCreatePath = '/recipes/create';
+  
+
+  static const String splashName = 'splash';
+  static const String homeName = 'home';
+  static const String recipesName = 'recipes';
+  static const String plannerName = 'planner';
+  static const String loginName = 'login';
+  static const String registerName = 'register';
+  static const String verifyEmailName = 'verify-email';
+  static const String subCategoryName = 'subCategory';
+  static const String recipeCreateName = 'recipe_create';
+}
