c8323394bafad7e1e830832e531588c328f3264d
Cill Fajardo <cillfroilanf@gmail.com>
2026-03-14 15:04:01 +0800

    Remove redundant comments from cookingDatabaseFacadeProvider in cooking_database_provider.dart

--- lib/features/library/cooking_database_provider.dart
+++ lib/features/library/cooking_database_provider.dart
@@ -1,8 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nutricook/services/cooking_database_facade.dart';
 
-// Provider for Cooking DB Facade Service to aggregate access to all library data
-// Mostly used for recipes and planner
 final cookingDatabaseFacadeProvider =
     Provider<CookingDatabaseFacadeService>((ref) {
   return CookingDatabaseFacadeService();
