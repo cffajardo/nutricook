9484c0c786bea06b52d7c5892674795b2fa6d209
Cill Fajardo <cillfroilanf@gmail.com>
2026-03-14 14:58:14 +0800

    Load environment variables using dotenv in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,18 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart'
     show SystemChrome, SystemUiOverlayStyle, SystemUiMode;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:nutricook/core/theme/app_theme.dart';
 import 'package:nutricook/features/profile/provider/user_preferences_provider.dart';
 import 'firebase_options.dart';
 import 'routing/main_router.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_ai/firebase_ai.dart';
-import 'firebase_options.dart';
 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  await dotenv.load(fileName: '.env');
+
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       systemNavigationBarColor: Colors.transparent,
